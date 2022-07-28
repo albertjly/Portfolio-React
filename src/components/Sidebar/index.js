f4f6f9c19dc872cd9daf300ad79d698cb1982d7f
Albert Jiang <albertjly@gmail.com>
2022-07-27 22:34:43 -0700

    add social media icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,8 @@ import { Link, NavLink } from 'react-router-dom';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCamera, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faSkype } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
   <div className='nav-bar'>
@@ -22,6 +23,28 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4d' />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/luyang-albert-jiang/'>
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel='noreferrer' href='https://github.com/albertjly'>
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel='noreferrer' href='https://www.albertjiang.com/'>
+          <FontAwesomeIcon icon={faCamera} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel='noreferrer' href='https://join.skype.com/invite/AzUx65i4XVOg'>
+          <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div >
 )
 
