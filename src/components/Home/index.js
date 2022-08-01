7392365f581cc9d3382061dc28cfb8271288898b
Albert Jiang <albertjly@gmail.com>
2022-08-01 01:08:26 -0700

    add pacman loader

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,8 @@ import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+import Loader from 'react-loaders';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -16,26 +18,30 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
 
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
-          <br />
-          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
-        </h1>
-        <h2>
-          Frontend Developer / JavaScript Expert
-        </h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+          </h1>
+          <h2>
+            Frontend Developer / JavaScript Expert
+          </h2>
+          <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        </div>
+        <Logo />
       </div>
-    </div>
+      <Loader type='pacman' />
+    </>
   )
 }
 
